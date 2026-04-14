6ee62cab4087314a99cfe7934eb343afb822779c
shwetaypatil <shwetaypatil05@gmail.com>
2026-04-14 19:22:15 +0530

    changed files

--- backend/config.py
+++ backend/config.py
@@ -1,11 +1,17 @@
+# import os
+
+# BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+# INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
+
+# # Create instance folder if it doesn't exist
+# os.makedirs(INSTANCE_DIR, exist_ok=True)
+
+# class Config:
+#     SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(INSTANCE_DIR, "db.sqlite3")
+#     SQLALCHEMY_TRACK_MODIFICATIONS = False
+
 import os
 
-BASE_DIR = os.path.abspath(os.path.dirname(__file__))
-INSTANCE_DIR = os.path.join(BASE_DIR, "instance")
-
-# Create instance folder if it doesn't exist
-os.makedirs(INSTANCE_DIR, exist_ok=True)
-
 class Config:
-    SQLALCHEMY_DATABASE_URI = "sqlite:///" + os.path.join(INSTANCE_DIR, "db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.environ.get("DATABASE_URL")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
