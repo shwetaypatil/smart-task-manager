a4dce066850a829648d93965d26eba84b7af609e
shwetaypatil <shwetaypatil05@gmail.com>
2026-04-16 18:00:40 +0530

    fixed port problem

--- backend/app.py
+++ backend/app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from config import Config
@@ -81,4 +82,5 @@ def get_summary():
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
